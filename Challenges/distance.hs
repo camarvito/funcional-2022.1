90bc639a2f8919e8de05f4e9b6dff3724e293af3
camarvito <victormacgrath@gmail.com>
2022-07-14 09:46:28 -0300

    fixed distance challenge

--- Challenges/distance.hs
+++ Challenges/distance.hs
@@ -2,9 +2,9 @@ import Data.List
 import Data.Maybe
 
 neib :: [a] -> Int -> Int -> [a]
-neib xs index lim = drop lower . take upper $ xs
-    where lower = max 0 (index - lim)
-          upper = min (length xs) (index + lim + 1)
+neib xs i lim = drop lower . take upper $ xs
+    where lower = max 0 (i - lim)
+          upper = min (length xs) (i + lim + 1)
 
 -- verifica se o valor existe no vetor
 exists :: Eq a => a -> [a] -> Bool
@@ -22,27 +22,34 @@ fit (xs, lim) index value = not . exists (dig2char value) $ neib xs index lim
 getHoles :: String -> [Int]
 getHoles xs = [x | (x, i) <- zip [0..] xs, i == '.']
 
--- -- insere esse valor nesse index e retorna o novo vetor resultante PS
-set :: String -> Int -> Char -> String
-set xs index v = take index xs ++ [v] ++ drop (index + 1) xs
+-- -- insere esse valor nesse index e retorna o novo vetor resultante
+set :: String -> Int -> Int -> String
+set xs i x = take i xs ++ [dig2char x] ++ drop (i + 1) xs
 
 -- -- tenta resolver o problema para essa posição
 -- -- se é possível resolver, retorna Just resposta, senão Nothing
 -- -- problema (xs, lim)
 -- -- holes: lista de posições a serem preenchidas
 -- -- hindex: posicao atual no vetor de holes
--- solve :: (String, Int) -> [Int] -> Int -> Maybe String
--- -- solve (xs, lim) holes hindex = ...
+solve :: (String, Int) -> [Int] -> Int -> Maybe String
+solve (xs, lim) holes hindex
+  | hindex == length holes = Just xs
+  | null solved = Nothing
+  | otherwise = head solved
+  where
+    p = holes !! hindex
+    tests = [solve (set xs p v, lim) holes (hindex + 1) | v <- [0 .. lim], fit (xs, lim) p v]
+    solved = [x | x <- tests, isJust x]
 
 -- -- prepara a entrada para a função recursiva de resolução
--- mainSolver :: String -> Int -> String
--- -- mainSolver xs lim = ...
+mainSolver :: String -> Int -> String
+mainSolver xs lim = fromJust $ solve (xs, lim) (getHoles xs) 0
 
--- main :: IO ()
--- main = do
---     xs <- getLine
---     lim <- readLn :: IO Int
---     putStrLn $ mainSolver xs lim
+main :: IO ()
+main = do
+    xs <- getLine
+    lim <- readLn :: IO Int
+    putStrLn $ mainSolver xs lim
 
 -- ------------------------------------------------------------------------------------
 -- -- Testes para lhe auxiliar a construir a solução de forma mais segura
@@ -61,18 +68,18 @@ dig2charTest :: IO ()
 dig2charTest = do
     print $ map dig2char [0..9] == ['0'..'9']
 
--- setTest :: IO ()
--- setTest = do
---     print $ set "12345" 0 9 == "92345"
---     print $ set "12345" 1 9 == "19345"
---     print $ set "12345" 4 9 == "12349"
+setTest :: IO ()
+setTest = do
+    print $ set "12345" 0 9 == "92345"
+    print $ set "12345" 1 9 == "19345"
+    print $ set "12345" 4 9 == "12349"
 
--- fitTest :: IO ()
--- fitTest = do -- (fit ("12.345", 1) 2) se torna uma função curry faltando só uma var que seria o valor recebido do vetor
---     print $ map (fit ("12.345", 1) 2) [1,2,3,4,5] == [True, False, False, True, True]
---     print $ map (fit ("12.345", 2) 2) [1,2,3,4,5] == [False, False, False, False, True]
---     print $ map (fit ("12.345", 3) 2) [1,2,3,4,5] == [False, False, False, False, False]
---     print $ map (fit ("12345.", 4) 5) [1,2,3,4,5] == [True, False, False, False, False]
+fitTest :: IO ()
+fitTest = do 
+    print $ map (fit ("12.345", 1) 2) [1,2,3,4,5] == [True, False, False, True, True]
+    print $ map (fit ("12.345", 2) 2) [1,2,3,4,5] == [False, False, False, False, True]
+    print $ map (fit ("12.345", 3) 2) [1,2,3,4,5] == [False, False, False, False, False]
+    print $ map (fit ("12345.", 4) 5) [1,2,3,4,5] == [True, False, False, False, False]
 
 getHolesTest :: IO ()
 getHolesTest = do
@@ -80,10 +87,10 @@ getHolesTest = do
     print $ getHoles "12.3.4" == [2,4]
     print $ getHoles "...3.4" == [0,1,2,4]
 
--- mainTest :: IO ()
--- mainTest = do
---     print $ mainSolver "01.2." 3 == "01320"
---     print $ mainSolver ".0..231..5" 5 == "1045231045"
---     print $ mainSolver "2..0..............3..........." 3 == "213021302130213021302130213021"
---     print $ mainSolver "0..32..41." 5 == "0413250413"
---     print $ mainSolver "9....7.620.5318....." 9 == "95318746209531874620"
+mainTest :: IO ()
+mainTest = do
+    print $ mainSolver "01.2." 3 == "01320"
+    print $ mainSolver ".0..231..5" 5 == "1045231045"
+    print $ mainSolver "2..0..............3..........." 3 == "213021302130213021302130213021"
+    print $ mainSolver "0..32..41." 5 == "0413250413"
+    print $ mainSolver "9....7.620.5318....." 9 == "95318746209531874620"
